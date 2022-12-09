360c5bf599f07ba9b81d702d926b59a0da6cb9a7
omriv88 <113102456+omriv88@users.noreply.github.com>
2022-12-09 12:05:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
 provider "aws" {
     region = "us-east-1"
-    profile = var.profile
 }
 
 
 resource "aws_instance" "wordpress" {
-    ami = "ami-02b972fec07f1e659"
+    ami = "ami-0b0dcb5067f052a63"
     instance_type = "t2.micro"
     key_name = "wordpress"
     vpc_security_group_ids = [ "sg-02bd12ea337b24edb" ]
 
     tags = {
-        Name = var.name
-        group = var.group
+        Name = "wordpress"
     }
 }
